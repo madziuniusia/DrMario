bab459f080d5555772e63862d051aa3ab6215492
madziuniusia <115087008+madziuniusia@users.noreply.github.com>
2023-04-10 13:23:23 +0200

    movement

--- src/classes/pill.ts
+++ src/classes/pill.ts
@@ -1,67 +1,92 @@
 import { halfPill } from "./halfpill.js";
-
 export class Pill {
-  wholePill: { pill1: any; pill2: any };
+  wholePill: { pill1?: any; pill2: any };
   blocked: boolean;
   arrayCellColor: any;
+  fall: any;
   constructor(arrayCellColor: any) {
     this.blocked = false;
     this.wholePill = { pill1: new halfPill(3, 0), pill2: new halfPill(4, 0) };
     this.arrayCellColor = arrayCellColor;
-    this.fallen();
-    this.turn();
+    this.interval(500);
+    this.movement();
+  }
+  interval(x: number) {
+    let pill1 = this.wholePill.pill1;
+    let pill2 = this.wholePill.pill2;
+    this.fall = setInterval(() => {
+      pill1.y++;
+      pill2.y++;
+      if (
+        pill1.y === 15 ||
+        pill2.y === 15 ||
+        this.arrayCellColor[pill1.y + 1][pill1.x] != "" ||
+        this.arrayCellColor[pill2.y + 1][pill2.x] != ""
+      ) {
+        this.blocked = true;
+        clearInterval(this.fall);
+      }
+    }, x);
   }
 
-  fallen() {
-    if (this.blocked === false) {
-      let pill1 = this.wholePill.pill1;
-      let pill2 = this.wholePill.pill2;
-      let fall = setInterval(() => {
-        pill1.y++;
-        pill2.y++;
-        if (
-          pill1.y === 15 ||
-          pill2.y === 15 ||
-          this.arrayCellColor[pill1.y + 1][pill1.x] != "" ||
-          this.arrayCellColor[pill2.y + 1][pill2.x] != ""
-        ) {
-          this.blocked = true;
-          clearInterval(fall);
-        }
-      }, 500);
-    }
-  }
-
-  turn() {
-    if (this.blocked === false) {
-      document.addEventListener("keydown", (e) => {
-        let conditionsBoard =
-          this.wholePill.pill1.x > 0 &&
-          this.wholePill.pill1.x < 7 &&
-          this.wholePill.pill2.x > 0 &&
-          this.wholePill.pill2.x < 7;
-        console.log(this.wholePill.pill2.x);
+  movement() {
+    document.addEventListener("keydown", (e) => {
+      if (this.blocked === false) {
+        let dx1 = 0,
+          dx2 = 0,
+          dy1 = 0,
+          dy2 = 0,
+          pill1 = this.wholePill.pill1,
+          pill2 = this.wholePill.pill2;
 
         if (e.key === "a" || e.key === "ArrowLeft") {
-          if (conditionsBoard) {
-            this.wholePill.pill1.x--;
-            this.wholePill.pill2.x--;
-          }
+          dx1 = dx2 = -1;
         } else if (e.key === "d" || e.key === "ArrowRight") {
-          if (conditionsBoard) {
-            this.wholePill.pill1.x++;
-            this.wholePill.pill2.x++;
+          dx1 = dx2 = 1;
+        } else if (e.key === "w" || e.key === "ArrowUp") {
+          if (pill1.x === pill2.x) {
+            if (pill1.y < pill2.y) {
+              dx2 = 1;
+              dy2 = -1;
+            } else {
+              dx2 = -1;
+              dy2 = 1;
+            }
+          } else {
+            if (pill1.x > pill2.x) dx2 = dy2 = 1;
+            else dx2 = dy2 = -1;
           }
-        } else if (e.key === "w" || e.key === "Shift") {
-          if (conditionsBoard) {
-            //this.wholePill.pill2.x
-            //obracanie
+        } else if (e.key == "Shift") {
+          if (pill1.x === pill2.x) {
+            if (pill1.y < pill2.y) dx2 = dy2 = -1;
+            else dx2 = dy2 = 1;
+          } else {
+            if (pill1.x > pill2.x) {
+              dx2 = 1;
+              dy2 = -1;
+            } else {
+              dx2 = -1;
+              dy2 = 1;
+            }
           }
         } else if (e.key === "s" || e.key == "ArrowDown") {
-          this.blocked === true;
-          //spadanie
+          clearInterval(this.fall);
+          this.interval(50);
         }
-      });
-    }
+        if (
+          pill1.x + dx1 >= 0 &&
+          pill1.x + dx1 <= 7 &&
+          pill2.x + dx2 >= 0 &&
+          pill2.x + dx2 <= 7 &&
+          pill1.y + dy1 >= 0 &&
+          pill2.y + dy2 >= 0
+        ) {
+          pill1.x += dx1;
+          pill2.x += dx2;
+          pill1.y += dy1;
+          pill2.y += dy2;
+        }
+      }
+    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { halfPill } from "./classes/halfpill.js";
-import { Pill } from "./classes//pill.js";
 import { Board } from "./classes/board.js";
 
 let ClassGame = new Board();
